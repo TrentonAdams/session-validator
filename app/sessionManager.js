4b62d017b04022832b2c58041bc1f401270a09a1
Trenton D. Adams <trent@trentonadams.ca>
2017-04-27 21:21:17 -0600

    - re-arrange session manager just a bit - implement session manager jasmine tests

--- app/sessionManager.js
+++ app/sessionManager.js
@@ -10,9 +10,10 @@ class SessionManager
   constructor()
   {
     this.randomNumber = Math.random();
+    this.instanceRequests = 1;
     this.session = {
       sessionValid: true,
-      sessionTime: 600
+      sessionTime: 600,
     };
     this.terminate = false;
   }
@@ -25,8 +26,8 @@ class SessionManager
       clearTimeout(sessionManager.intervalVariable);
       sessionManager.intervalVariable = undefined;
       sessionManager.terminate = false;
-      sessionManager.session.sessionValid = true;
-      sessionManager.session.sessionTime = 600;
+      sessionManager.session.sessionValid = false;
+      sessionManager.session.sessionTime = 0;
       return;
     }
 
@@ -39,13 +40,15 @@ class SessionManager
     if (this.intervalVariable === undefined)
     {
       console.log("setting up session timer");
+      sessionManager.session.sessionValid = true;
+      sessionManager.session.sessionTime = 600;
       this.intervalVariable = setInterval(this.sessionCounter, 1000);
     }
   };
 
   expireSession()
   {
-    console.log("expiring session timer");
+    //console.log("expiring session timer");
     this.session = {
       sessionValid: false,
       sessionTime: 0
@@ -57,13 +60,13 @@ class SessionManager
 let sessionManager = undefined;
 if (sessionManager === undefined)
 {
-
-  console.log('construct SessionManager');
+  //console.log('construct SessionManager');
   sessionManager = new SessionManager();
 }
 else
 {
-  console.log("Not constructing session manager" + sessionManager);
+  sessionManager.instanceRequests++;
+  //console.log("Not constructing session manager" + sessionManager);
 }
 
 console.log("session manager id: " + sessionManager.randomNumber);
---
+++ spec/sessionManagerSpec.js
@@ -0,0 +1,27 @@
+describe("SessionManager tests", function ()
+{
+  let sessionManager = require('../app/sessionManager').SessionManager;
+  sessionManager.startSession();
+
+  it("SessionTimer running", function (done)
+  {
+    setTimeout(function()
+    {
+      expect(sessionManager.session.sessionValid).toBe(true);
+      expect(sessionManager.session.sessionTime).toEqual(599);
+      done();
+    }, 1020);
+  });
+
+  it("Expire Session working", function (done)
+  {
+    sessionManager.expireSession();
+    expect(sessionManager.session.sessionTime).toEqual(0);
+    expect(sessionManager.session.sessionValid).toBe(false);
+    setTimeout(function()
+    {
+      expect(sessionManager.terminate).toBe(false);
+      done();
+    }, 1000);
+  });
+});
