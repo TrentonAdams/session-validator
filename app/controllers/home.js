51f3dd816f604aaf1910dd57a4f5ad12ba1cf0f3
Trenton D. Adams <trent@trentonadams.ca>
2017-04-27 20:10:22 -0600

    - handle json response for main page

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -13,12 +13,23 @@ router.get('/', function (req, res, next)
 {
   Article.find(function (err, articles)
   {
+    let body = {
+      title: 'Generator-Express MVC',
+      articles: articles
+    };
     if (err) return next(err);
     console.log(sessionManager);
     sessionManager.startSession();
-    res.render('index', {
-      title: 'Generator-Express MVC',
-      articles: articles
-    });
+
+    // handle html and json responses, so that unit tests can get a sane
+    // response.
+    if (req.accepts('json'))
+    {
+      res.send(body);
+    }
+    else
+    {
+      res.render('index', body);
+    }
   });
 });
